4ad5937a59626baa5d95b64cba62f257e0d33bbf
SHAHBAJ BEG <50695046+SB2505@users.noreply.github.com>
2021-02-16 19:04:19 +0530

    Update newcode.py

--- newcode.py
+++ newcode.py
@@ -10,8 +10,8 @@ uploaded_ecaps_file = st.file_uploader("Upload ecaps Files",type=['xlsx'])
 # if uploaded_file is not None:
 #     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
 #     st.write(file_details)
-df1=pd.read_excel(uploaded_razorpay_file)
-df2=pd.read_excel(uploaded_ecaps_file)
+df1=pd.read_excel(uploaded_razorpay_file,engine='openpyxl')
+df2=pd.read_excel(uploaded_ecaps_file,engine='openpyxl')
 A=df1['SettlementAmount'].count()
 B=df2['amount'].count()
 C=df2['amount'].sum()
